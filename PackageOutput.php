6c3e7a38854b4aa93b09d85ac263d8d8eace810c
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-01-25 15:15:02 +1100

    Refactored

---
+++ PackageOutput.php
@@ -0,0 +1,32 @@
+<?php
+
+// Enrico Simonetti
+// enricosimonetti.com
+//
+// 2018-01-25 on Sugar 7.9.3.0
+//
+// Tool that helps package Sugar installable modules
+
+class PackageOutput
+{
+    public static function message($out = '')
+    {
+        if (!empty($out)) {
+            echo $out . PHP_EOL;
+        }
+    }
+
+    public static function createFile($filename = '', $content = '')
+    {
+        if (!empty($filename)) {
+            file_put_contents($filename, $content);
+        }
+    }
+
+    public static function copyFile($src, $dst)
+    {
+        if (!empty($src) && !empty($dst)) {
+            copy($src, $dst);
+        }
+    }
+}
